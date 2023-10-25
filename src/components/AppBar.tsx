510a5b299dc051dd72ab0e1b2a8bb55e69fbcabc
Tobias Schmid <tobiasschmid3@gmx.de>
2023-10-25 09:09:47 +0200

    new home screen items

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,9 +1,25 @@
-import { IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonHeader, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { useMemo } from 'react';
 
 const AppBar: React.FC = () => {
+  const router = useIonRouter();
+  const backTarget = useMemo(()=>{
+    if (router.routeInfo.lastPathname && router.routeInfo.pathname !== "/home") {
+      return router.routeInfo.lastPathname;
+    }
+    switch(router.routeInfo.pathname) {
+      case "/scales": return "/home";
+      default: return "";
+    }
+  }, [router.routeInfo.pathname]);
   return (
     <IonHeader>
         <IonToolbar>
+          {
+            backTarget && (<IonButtons slot="start">
+              <IonBackButton defaultHref={backTarget}></IonBackButton>
+              </IonButtons>)
+          }
           <IonTitle className="ion-text-center">Beefone</IonTitle>
         </IonToolbar>
       </IonHeader>
