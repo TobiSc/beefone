342ce9da6ddcbc85dcec6be67064922181926ced
Tobias Schmid <tobiasschmid3@gmx.de>
2023-12-30 22:28:39 +0100

    Waagenimport hinzugefügt

--- dataImport.js
+++ dataImport.js
@@ -18,22 +18,25 @@ export const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
 
+async function importScaleData() {
+    fs.readFile('./999data.csv', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        let records = parse(data, { delimiter: ";" })
+        let filtered = records.filter((record, index) => index % 120 === 0);
+        upsertRecords(filtered);
 
-fs.readFile('./999data.csv', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    let records = parse(data, { delimiter: ";" })
-    let filtered = records.filter((record, index) => index % 120 === 0);
-    upsertRecords(filtered);
+    });
+}
 
-});
+//importScaleData();
 
 async function upsertRecords(records) {
     for (let [index, record] of records.entries()) {
         let timestamp = new Date(record[0] + ":" + record[1]).valueOf();  //unix timestamp
-        let scaleDataRef = doc(firestore, "scaleData/" + timestamp.toString())
+        let scaleDataRef = doc(firestore, "scaleData/" + record[3] + "-" + timestamp.toString())
         await setDoc(scaleDataRef, {
             Date: record[0],
             Time: record[1],
@@ -52,3 +55,45 @@ async function upsertRecords(records) {
         console.log(index + "/" + records.length)
     }
 }
+
+const scales = [
+    [
+        999, 85.971, 85.906, 87.233, 86.55, 0.00252075, -0.000718158, 0.00285217, -0.00263692, 146288, 259674, 156422, 18194
+    ],
+    [
+        1002, 83.673, 84.113, 84.113, 84.725, 0, 0, 0, 0, 229710, 18655, 66589, -1652
+    ],
+    [
+        1003, 86.393, 84.055, 84.832, 85.171, 0, 0, 0, 0, -107955, -58024, -55196, -6866
+    ],
+    [
+        1004, 86.803, 87.125, 86.344, 87.087, 0, 0, 0, 0, 229589, 14431, 306097, 109896
+    ],
+    [
+        997, 11.22, 11.22, 11.22, 11.22, 0, 0, 0, 0, 1000000, 1000000, 1000000, 1000000
+    ]
+]
+
+async function importScales() {
+    for (let scale of scales) {
+        let scaleRef = doc(firestore, "scales/" + scale[0])
+        let data = {
+            serial: scale[0],
+            sc1mcoeffc: scale[1],
+            sc2mcoeffc: scale[2],
+            sc3mcoeffc: scale[3],
+            sc4mcoeffc: scale[4],
+            sc1tcoeffc: scale[5],
+            sc2tcoeffc: scale[6],
+            sc3tcoeffc: scale[7],
+            sc4tcoeffc: scale[8],
+            sc1tara: scale[9],
+            sc2tara: scale[10],
+            sc3tara: scale[11],
+            sc4tara: scale[12],
+        }
+        await setDoc(scaleRef, data, { merge: true }).catch((e) => console.error(e));
+    }
+}
+
+//importScales();
