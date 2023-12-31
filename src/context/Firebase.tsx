07fdae305a6681f50e1b0eb9987901cfc3fc3024
Tobias Schmid <tobiasschmid3@gmx.de>
2023-12-31 11:38:01 +0100

    Offline-Daten aktiviert

--- src/context/Firebase.tsx
+++ src/context/Firebase.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { connectAuthEmulator, getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +18,10 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 //export const auth = getAuth(app);
-export const firestore = getFirestore();
-connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
+export const firestore = initializeFirestore(app, {
+  localCache:
+    persistentLocalCache(/*settings*/{ tabManager: persistentMultipleTabManager() })
+})
+
+//connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
 //connectAuthEmulator(auth, "http://127.0.0.1:9099");
